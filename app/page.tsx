2f3a904301a866602d076a6859f660bf0252c88b
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2025-05-31 13:10:28 -0500

    Basic main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
+import MainTitle from "@/MainTitle";
+import { Container, Text } from "@mantine/core";
+
 export default function HomePage() {
-  return <div>Home page</div>;
+  return (
+    <Container mt={"xl"}>
+      <MainTitle title="Desarrollo de aplicaciones en red: Laboratorio 2" />
+      <Text>
+        Esta es una pagina con los ejercicios de la materia de "Desarrollo de
+        aplicaciones en Red", relacionadas al Laboratorio 2: Tecnologías
+        JavaScript y AJAX
+      </Text>
+      <Text>Usando el menu, se puede acceder a diferentes ejercicios</Text>
+    </Container>
+  );
 }
