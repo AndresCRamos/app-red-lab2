3fa9712cb7ee7ec33aea60771f0dc227bbdd007c
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2025-06-02 22:40:21 -0500

    Update next js config

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,13 @@ const nextConfig = {
   experimental: {
     optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
   },
+  output: "export",
+  
+  basePath: "/app-red-lab2",
+  assetPrefix: "/app-red-lab2/", 
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
