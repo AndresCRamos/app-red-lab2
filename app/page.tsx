3545422e35dfbf04507c576115ca00dcffd17648
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2025-06-02 22:45:22 -0500

    Fix quotes error

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ export default function HomePage() {
     <Container mt={"xl"}>
       <MainTitle title="Desarrollo de aplicaciones en red: Laboratorio 2" />
       <Text>
-        Esta es una pagina con los ejercicios de la materia de "Desarrollo de
-        aplicaciones en Red", relacionadas al Laboratorio 2: Tecnologías
+        Esta es una pagina con los ejercicios de la materia de Desarrollo de
+        aplicaciones en Red, relacionadas al Laboratorio 2: Tecnologías
         JavaScript y AJAX
       </Text>
       <Text>Usando el menu, se puede acceder a diferentes ejercicios</Text>
